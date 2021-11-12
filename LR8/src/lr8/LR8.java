62027cfd7cf96bed76f1ecb62b5d994574312437
Elena <kozlova03@inbox.ru>
2021-11-12 22:12:16 +0700

    Удалены лишние комментарии

--- LR8/src/lr8/LR8.java
+++ LR8/src/lr8/LR8.java
@@ -1,22 +1,17 @@
 package lr8;
-
+import java.util.*;
 public class LR8 {
 
     public static void main(String[] args) {
-        Food food1 = new Food(); List_cow list_cow1 = new List_cow();
-        List_cow list_cow2 = new List_cow();//Для стада коров
-        Cowshed cowshed1 = new Cowshed();
-        System.out.println("\n Количество коровников на ферме: " + Cowshed.GetKolVo());
-        Cowshed cowshed2 = new Cowshed(); Cowshed cowshed3 = new Cowshed();
-        System.out.println("\n *Добавлено два коровника*");
-        System.out.println("\n Количество коровников на ферме: " + Cowshed.GetKolVo());
         
-        
-        
-        food1.set("Свёкла"); food1.set("Kомбикорм"); food1.set("Силос");
-        food1.set();   
-        for (int i = 0; i < 2; i++) list_cow1.add(food1);
-		list_cow1.get_print_list();
-        
+   Scanner inp = new Scanner(System.in);    
+   int a[] = new int[10]; 
+    for (int i = 0; i < 10; i++) 
+    {
+         System.out.println(" Место № " + (i + 1) + "\n Введите 1, чтобы обозначить, что место занято коровой\n 0 - если место свободно : "); 
+         a[i] = inp.nextInt();
+    }
+    Cowshed cowshed = new Cowshed(a);
+    cowshed.PrintZanyatMest();     
     }
 }
