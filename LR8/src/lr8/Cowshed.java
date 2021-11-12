931d00a19c95a76a344bca835d2c2bf4cc214b8f
Elena <kozlova03@inbox.ru>
2021-11-12 16:34:46 +0700

    В классе Cowshed добавлен конструктор, инициализирующий массив zanyatMest()

--- LR8/src/lr8/Cowshed.java
+++ LR8/src/lr8/Cowshed.java
@@ -5,6 +5,17 @@ public class Cowshed {
         {
             KolVoCowshed++;
         }
+    public Cowshed(int zanyatMest[])
+	{
+	System.out.println("\n *Запуск конструктора c одним параметром класса Cowshed*\n");
+	for (int i = 0; i < 10; i++) if ((zanyatMest[i] == 0)||(zanyatMest[i] == 1)) this.zanyatMest[i] = zanyatMest[i];
+        else if (zanyatMest[i] < 0) this.zanyatMest[i] = 0;
+        else if (zanyatMest[i] > 0) this.zanyatMest[i] = 1;
+	length = 500;
+	width = 500;
+	height = 3;
+	KolVoCowshed++;
+	}
         public void Set(int length, int width, int height)
         {
             this.length = length;
@@ -21,9 +32,15 @@ public class Cowshed {
         {
             return KolVoCowshed;
         }
-        private static int KolVoCowshed = 0;
-        private int length = 500;
-        private int width = 500;
-        private int height = 3;
+        public void PrintZanyatMest()
+	{
+		System.out.println("\n 1 - место занято коровой\n 0 - место свободно\n Наполненность коровника следующая:\n");
+		for (int i = 0; i < 10; i++)  System.out.print(" " + zanyatMest[i]); System.out.println("\n ");
+	}
+        private static int KolVoCowshed;
+        private int length;
+        private int width;
+        private int height;
+        private int[] zanyatMest = new int[10];
     
 }
