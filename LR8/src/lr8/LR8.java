51428eaf2e9ed75425a9ea5419658a415a9bc3b6
Elena <kozlova03@inbox.ru>
2021-11-20 23:21:04 +0700

    Добавление защищенных блоков, перехватов исключений и инициализации исключений в метод Set класса Cow

--- LR8/src/lr8/LR8.java
+++ LR8/src/lr8/LR8.java
@@ -3,15 +3,16 @@ import java.util.*;
 public class LR8 {
 
     public static void main(String[] args) {
-        
-   Scanner inp = new Scanner(System.in);    
-   int a[] = new int[10]; 
-    for (int i = 0; i < 10; i++) 
-    {
-         System.out.println(" Место № " + (i + 1) + "\n Введите 1, чтобы обозначить, что место занято коровой\n 0 - если место свободно : "); 
-         a[i] = inp.nextInt();
-    }
-    Cowshed cowshed = new Cowshed(a);
-    cowshed.PrintZanyatMest();     
+        List_cow list_cow = new List_cow();
+        Food food = new Food();
+        food.Add("Силос"); food.Add("Комбикорм"); food.Add("Солома"); food.Add("Свёкла");
+        try
+        {
+            list_cow.add(food);
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
     }
 }
