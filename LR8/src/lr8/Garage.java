ebed0aec144b03626ca5fe595bfe9b9fd3f37e45
Elena <kozlova03@inbox.ru>
2021-11-27 22:58:33 +0700

    Для класса Building добавлена перегрузка метода toString

--- LR8/src/lr8/Garage.java
+++ LR8/src/lr8/Garage.java
@@ -1,21 +1,22 @@
 package lr8;
 
 public class Garage extends Building{
-    	Garage()
+    	public Garage()
 	{
 		kolVoCars = 15;
 	}
-	Garage(int length, int width, int height, int kolVoFloor, int kolVoCars)
+	public Garage(int length, int width, int height, int kolVoFloor, int kolVoCars)
 	{
                 super(length, width, height, kolVoFloor); 
+                System.out.println("\n *Вызов конструктора производного класса с парамерами*");
 		this.kolVoCars = kolVoCars;
 	}
-	void Set(int length, int width, int height, int kolVoFloor, int kolVoCars)
+	public void Set(int length, int width, int height, int kolVoFloor, int kolVoCars)
 	{
 		super.Set(length, width, height, kolVoFloor);
 		this.kolVoCars = kolVoCars;
 	}
-	void Print()
+	public void Print()
 	{
 		System.out.println("\n Длина гаража: " + length + "\n Ширина гаража: " + width + "\n Высота гаража: " + height + "\n Количество этажей: " + kolVoFloor + "\n Количество машиномест: " + kolVoCars);
 	}
