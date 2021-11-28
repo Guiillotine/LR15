498f401e47fda53b111bf9c8480bac21b26bffba
Elena <kozlova03@inbox.ru>
2021-11-28 15:18:41 +0700

    Добавлены производные от класса Worker классы Cleaner, Admin, Director

---
+++ LR8/src/lr8/Director.java
@@ -0,0 +1,23 @@
+package lr8;
+class Director extends Worker {
+    public Director()
+	{
+	}
+    public Director(String name, String surname, int age, char sex, int oklad, int pooscher)
+	{
+                super(name, surname, age, sex, oklad); 
+                this.pooscher = pooscher;
+	}
+    public void Print()
+	{
+		System.out.printf("\n Имя: " + name + "\n Фамилия: " + surname + "\n Возраст: " + age + "\n Пол: " + sex + "\n Оклад: " + oklad + "\n Районный коэффициент: " + rayKoef + " \n Премия: " + prem + "\n Поощерительные по результатам работы: " + pooscher);
+	}
+    public int ZarPlat()
+    {
+    return(oklad + oklad * rayKoef / 100 + oklad * prem / 100 + oklad * pooscher / 100);
+    }
+
+    private int rayKoef = 15;
+    private int prem = 20; 
+    private int pooscher;  
+}
