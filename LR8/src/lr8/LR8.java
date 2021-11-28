d20099863272896f2c0d798d5cd41f79ec8f0977
Elena <kozlova03@inbox.ru>
2021-11-28 19:16:22 +0700

    Для объектов класса Garage предусмотрено глубокое клонирование

--- LR8/src/lr8/LR8.java
+++ LR8/src/lr8/LR8.java
@@ -3,14 +3,19 @@ import java.util.*;
 public class LR8 {
 
     public static void main(String[] args) {
-        List_cow list_cow = new List_cow();
         Food food = new Food();
         food.Add("Силос"); food.Add("Комбикорм"); food.Add("Солома"); food.Add("Свёкла");
-        Building building = new Building(60, 15, 8, 2);
-        Garage garage = new Garage(20, 10, 4, 1, 25);
-	System.out.printf("%s",building.toString());
+        Garage a = new Garage(20,10,4,1,25);
+        Garage b = new Garage(15,15,4,1,20);
         System.out.printf("\n");
-        System.out.printf("%s",garage.toString());
+        System.out.printf("\n ГАРАЖ 1:"); a.Print();
+        System.out.printf("\n ГАРАЖ 2:"); b.Print();
+        b=(Garage)a.clone();       
+        b.Set(19, 9, 4, 1,25);
+        System.out.printf("\n");
+        System.out.printf("\n ГАРАЖ 1:");a.Print();
+        System.out.printf("\n ГАРАЖ 2:");b.Print();       
+        
         System.out.printf("\n");
 
         
