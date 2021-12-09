8f56a63e11cd40f8d5717d3353a1dd97eb0345df
Elena <kozlova03@inbox.ru>
2021-12-09 21:48:34 +0700

    Демонстрация в main

--- LR8/src/lr8/LR8.java
+++ LR8/src/lr8/LR8.java
@@ -4,30 +4,34 @@ public class LR8 {
 
     public static void main(String[] args) {
         Food food = new Food();
-        food.Add("Силос"); food.Add("Комбикорм"); food.Add("Солома"); food.Add("Свёкла");
-        Garage a = new Garage(20,10,4,1,25);
-        Garage b = new Garage(15,15,4,1,20);
-        System.out.printf("\n");
-        System.out.printf("\n ГАРАЖ 1:"); a.Print();
-        System.out.printf("\n ГАРАЖ 2:"); b.Print();
-        b=(Garage)a.clone();       
-        b.Set(19, 9, 4, 1,25);
-        System.out.printf("\n");
-        System.out.printf("\n ГАРАЖ 1:");a.Print();
-        System.out.printf("\n ГАРАЖ 2:");b.Print();       
+        food.Add("Силос"); food.Add("Комбикорм"); food.Add("Солома"); food.Add("Свёкла");        
         
-        System.out.printf("\n");
+    Worker worker1 = new Worker("Иванов", "Иван", 60, 'М', 22000);
+    System.out.printf("\n РАБОТНИК 1:\n");
+    worker1.Print(); System.out.printf("\n");
+    Worker worker2 = new Worker("Бучнев", "Алексей", 42, 'М', 20000);
+    System.out.printf("\n РАБОТНИК 2:\n");
+    worker2.Print(); System.out.printf("\n"); 
 
+    Compare<Worker> A = new Compare<Worker>();
+    System.out.printf("\n Работник с большим окладом:\n");
+    if (A.Max(worker1, worker2)) worker1.Print(); else worker2.Print();
+    System.out.printf("\n\n Работник с меньшим окладом:\n");
+    if (A.Min(worker1, worker2)) worker1.Print(); else worker2.Print();
+
+    Director director1 = new Director("Борисов", "Борис", 65, 'М', 90000, 40);
+    System.out.printf("\n\n ДИРЕКТОР ПРЕДПРИЯТИЯ 1:\n"); director1.Print(); System.out.printf("\n");
+    Director director2 = new Director("Сидоров", "Сергей", 40, 'М', 60000, 20);
+    System.out.printf("\n ДИРЕКТОР ПРЕДПРИЯТИЯ 2:\n"); director2.Print(); System.out.printf("\n");
+
+    Compare<Director> B = new Compare<Director>();
+    System.out.printf("\n Директор с большим окладом:\n");
+    if (A.Max(director1, director2)) director1.Print(); else director2.Print();
+    System.out.printf("\n\n Директор с меньшим окладом:\n");
+    if (A.Min(director1, director2)) director1.Print(); else director2.Print();
         
-        /*for (int i = 0; i < 2; i++)
-        try
-        {
-            list_cow.add(food);
-        }
-        catch(Exception ex)
-        {
-            System.out.println(ex.getMessage());
-        }*/
-
+        
+    System.out.printf("\n");
+      
     }
 }
